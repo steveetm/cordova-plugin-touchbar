2cdb1aed47391ea3c12d79b4cf7a8ec34ce259cb
steveetm <steve@powerednow.com>
2017-09-25 08:25:57 +0200

    Remove extraneous cordova.define

--- www/plugin.js
+++ www/plugin.js
@@ -1,46 +1,41 @@
-cordova.define("cordova-plugin-touchbar.plugin", function (require, exports, module) {
+var exec = require('cordova/exec');
 
-    var exec = require('cordova/exec');
+var PLUGIN_NAME = 'TouchBarPlugin';
 
-    var PLUGIN_NAME = 'TouchBarPlugin';
+var TouchBar = {
+        _identifiers: {},
+        setDefaultItemIdentifiers: function (itemIdentifiers, cb) {
+            exec(cb, null, PLUGIN_NAME, 'setDefaultItemIdentifiers', [...itemIdentifiers]);
+        },
+        buttonEventHandler: function (eventObj) {
+            let pressedButton = Object.keys(this._identifiers).find((identifierKey) => {
+                return this._identifiers[identifierKey].identifier === eventObj.identifier
+            });
 
-    var TouchBar = {
-            _identifiers: {},
-            setDefaultItemIdentifiers: function (itemIdentifiers, cb) {
-                exec(cb, null, PLUGIN_NAME, 'setDefaultItemIdentifiers', [...itemIdentifiers]);
-            },
-            buttonEventHandler: function (eventObj) {
-                let pressedButton = Object.keys(this._identifiers).find((identifierKey) => {
-                    return this._identifiers[identifierKey].identifier === eventObj.identifier
-                });
-
-                if (pressedButton) {
-                    this._identifiers[pressedButton].onTap();
-                }
-            },
-            defineTouchBarItems: function (items, cb) {
-                items.forEach(item => this._identifiers[item.identifier] = item);
-                exec(cb, null, PLUGIN_NAME, 'defineTouchBarItems', items);
-                this._setCallbackHandler(this.buttonEventHandler.bind(this));
-
-            },
-            _setCallbackHandler: function (callback) {
-                exec(callback, null, PLUGIN_NAME, '_setCallbackHandler', []);
-            },
-            identifiers: {
-                otherItemsProxy:
-                    'NSTouchBarItemIdentifierOtherItemsProxy',
-                characterPicker:
-                    'NSTouchBarItemIdentifierCharacterPicker',
-                fixedSpaceSmall:
-                    'NSTouchBarItemIdentifierFixedSpaceSmall',
-                fixedSpaceLarge:
-                    'NSTouchBarItemIdentifierFixedSpaceLarge'
+            if (pressedButton) {
+                this._identifiers[pressedButton].onTap();
             }
+        },
+        defineTouchBarItems: function (items, cb) {
+            items.forEach(item => this._identifiers[item.identifier] = item);
+            exec(cb, null, PLUGIN_NAME, 'defineTouchBarItems', items);
+            this._setCallbackHandler(this.buttonEventHandler.bind(this));
+
+        },
+        _setCallbackHandler: function (callback) {
+            exec(callback, null, PLUGIN_NAME, '_setCallbackHandler', []);
+        },
+        identifiers: {
+            otherItemsProxy:
+                'NSTouchBarItemIdentifierOtherItemsProxy',
+            characterPicker:
+                'NSTouchBarItemIdentifierCharacterPicker',
+            fixedSpaceSmall:
+                'NSTouchBarItemIdentifierFixedSpaceSmall',
+            fixedSpaceLarge:
+                'NSTouchBarItemIdentifierFixedSpaceLarge'
         }
-    ;
+    };
 
-    module.exports = TouchBar;
+module.exports = TouchBar;
 
-})
-;
